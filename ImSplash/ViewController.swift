292f5781bb0dd395ebdbba7595765fc8a8a63be0
HoangDoan <hoang@zappasoft.com>
2020-04-21 09:06:00 +0700

    Initial the project with Pods

---
+++ ImSplash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImSplash
+//
+//  Created by Doan Minh Hoang on 4/21/20.
+//  Copyright © 2020 Doan Minh Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
